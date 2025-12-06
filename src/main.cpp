23dc87e88b1759357699c996936a73b685ca6e74
Mickylus <148538494+Mickylus@users.noreply.github.com>
2025-12-06 15:57:28 +0100

    Aggiunta la connsessione wifi e wifi(default)

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <SD.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+#include <WiFi.h>
 
 // Pin SD
 #define MOSI_SD 15
@@ -34,6 +35,7 @@ void setup(){
     }else{
         Serial.println("SD initialized.");
     }
+    connectWiFi();
     // Fornisce la pagina web
     server.onNotFound([](AsyncWebServerRequest *request){
         if(!serveFromSD(request)){
@@ -114,4 +116,85 @@ void aggiornaDati() {
     json += "\"volume\":" + String(vol);
     json += "}";
     writeFile("/dati.json", json);
+}
+
+bool readWifiConfig(String &ssid, String &password, String &d_ssid, String &d_password){
+    if(!SD.exists("/web/settings.json")){
+        Serial.println("settings.json non trovato");
+        return false;
+    }
+    File f = SD.open("/web/settings.json", FILE_READ);
+    if (!f){
+        Serial.println("Impossibile aprire settings.json");
+        return false;
+    }
+
+    String raw = f.readString();
+    f.close();
+
+    raw.replace("\n", "");
+    raw.replace("\r", "");
+    raw.replace(" ", "");
+
+    int ssidStart = raw.indexOf("\"ssid\":\"");
+    int passStart = raw.indexOf("\"password\":\"");
+    int dssidStart = raw.indexOf("\"d_ssid\":\"");
+    int dpassStart = raw.indexOf("\"d_password\":\"");
+
+    if (ssidStart < 0 || passStart < 0 || dssidStart < 0 || dpassStart < 0){
+        return false;
+    }
+    ssidStart += 8;  // salto "ssid":"  
+    passStart += 12; // salto "password":" 
+    dssidStart += 10;
+    dpassStart += 14;
+
+    int ssidEnd = raw.indexOf("\"", ssidStart);
+    int passEnd = raw.indexOf("\"", passStart);
+    int dssisEnd = raw.indexOf("\"", dssidStart);
+    int dpassEnd = raw.indexOf("\"", dpassStart);
+
+    ssid = raw.substring(ssidStart, ssidEnd);
+    password = raw.substring(passStart, passEnd);
+    d_ssid = raw.substring(dssidStart,dssisEnd);
+    d_password = raw.substring(dpassStart,dpassEnd);
+    return true;
+}
+
+void connectWiFi(){
+    String ssid, password, dssid,dpassword;
+    if(!readWifiConfig(ssid,password,dssid,dpassword)){
+        Serial.println("Nessun WiFi configurato.");
+        return;
+    }
+    Serial.println("Connessione a: " + ssid);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid.c_str(), password.c_str());
+    unsigned long start = millis();
+    while (WiFi.status() != WL_CONNECTED && millis() - start < 10000) {
+        delay(500);
+        Serial.print(".");
+    }
+    if(WiFi.status() == WL_CONNECTED) {
+        Serial.println("\nConnesso!");
+        Serial.print("IP: ");
+        Serial.println(WiFi.localIP());
+    }else{
+        Serial.println("\nConnessione a " + ssid + " fallita!");
+        Serial.println("Provo con il WiFi di default\n");
+        WiFi.mode(WIFI_STA);
+        WiFi.begin(dssid.c_str(), dpassword.c_str());
+        unsigned long start = millis();
+        while (WiFi.status() != WL_CONNECTED && millis() - start < 10000) {
+            delay(500);
+            Serial.print(".");
+        }
+        if(WiFi.status() == WL_CONNECTED) {
+            Serial.println("\nConnesso!");
+            Serial.print("IP: ");
+            Serial.println(WiFi.localIP());
+        }else{
+            Serial.println("Connessione a " + dssid +" fallita!");
+        }
+    }
 }
